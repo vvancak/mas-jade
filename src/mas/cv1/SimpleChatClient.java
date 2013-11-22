c54132e23ce2d08dc19ca69c484ff74e7f9fd99e
Martin Pilat <martin.pilat@gmail.com>
2013-11-22 18:36:22 +0100

    Added some comments to the sources.

--- src/mas/cv1/SimpleChatClient.java
+++ src/mas/cv1/SimpleChatClient.java
@@ -8,12 +8,15 @@ import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
 
+
+//jednoduchy agent, ktery kazdych 5 vterin posila zpravu prvnimu serveru, ktery najde
 public class SimpleChatClient extends Agent {
 
     @Override
     protected void setup() {
         super.setup();
 
+        //pridani chovani klienta -- posila zpravu serveru kazdych 5 vterin
         this.addBehaviour(new MsgSendingBehavior(this));
     }
 
@@ -22,6 +25,7 @@ public class SimpleChatClient extends Agent {
         super.takeDown();
     }
 
+    //jednoduche chovani, ktere kazdych 5 vterin posle zpravu serveru
     class MsgSendingBehavior extends TickerBehaviour {
 
         public MsgSendingBehavior(Agent a) {
@@ -31,18 +35,22 @@ public class SimpleChatClient extends Agent {
         @Override
         public void onTick() {
 
+            //vytvoreni popisu sluzby serveru
             ServiceDescription sd = new ServiceDescription();
             sd.setType("messaging-server");
 
+            //vytvoreni popisu agenta, ktery sluzbu poskytuje
             DFAgentDescription dfd = new DFAgentDescription();
             dfd.addServices(sd);
 
             try {
+                //vyhledani vsech serveru
                 DFAgentDescription[] servers = DFService.search(myAgent, dfd);
                 if (servers.length == 0) {
                     System.err.println("No servers found");
                     return;
                 }
+                //vytvoreni zpravy a poslani prvnimu serveru ze seznamu
                 ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
                 msg.addReceiver(servers[0].getName());
                 msg.setContent("Hi");
--- src/mas/cv1/SimpleChatServer.java
+++ src/mas/cv1/SimpleChatServer.java
@@ -10,26 +10,31 @@ import jade.lang.acl.ACLMessage;
 
 import java.util.Date;
 
+//jednoduchy agent, ktery se zaregistruje s DF a vypisuje prijate zpravy, zaroven je posila vsem klientum, ktere najde
 public class SimpleChatServer extends Agent {
 
     @Override
     protected void setup() {
         super.setup();
 
+        //popis sluzby messaging-server
         ServiceDescription sd = new ServiceDescription();
         sd.setType("messaging-server");
         sd.setName("server");
 
+        //popis tohoto agenta a sluzeb, ktere nabizi
         DFAgentDescription dfd = new DFAgentDescription();
         dfd.setName(this.getAID());
         dfd.addServices(sd);
 
+        //zaregistrovani s DF
         try {
             DFService.register(this, dfd);
         } catch (FIPAException e) {
             e.printStackTrace();
         }
 
+        //pridani chovani, ktere se stara o prijem, vypis a preposlani zprav
         this.addBehaviour(new MessageReceivingBehaviour());
     }
 
@@ -37,6 +42,7 @@ public class SimpleChatServer extends Agent {
     protected void takeDown() {
         super.takeDown();
 
+        //nezapomenout se nakonec odregistrovat z DF
         try {
             DFService.deregister(this);
         } catch (FIPAException e) {
@@ -44,6 +50,8 @@ public class SimpleChatServer extends Agent {
         }
     }
 
+
+    //chovani, ktere vypisuje prijate zpravy a posila je vsem klientum
     class MessageReceivingBehaviour extends CyclicBehaviour {
 
         @Override
@@ -52,19 +60,27 @@ public class SimpleChatServer extends Agent {
             if (msg == null)
                 return;
 
+            //vypis prijate zpravy
             System.out.println("[" + new Date().toString() + "] " + msg.getSender().getName() + ": " + msg.getContent());
 
+            //vytvoreni popisu sluzby messaging-client
             ServiceDescription sd = new ServiceDescription();
             sd.setType("messaging-client");
 
+            //vytvoreni popisu agenta, ktery poskytuje messaging-client
             DFAgentDescription dfd = new DFAgentDescription();
             dfd.addServices(sd);
 
-            ACLMessage toClients = new ACLMessage(ACLMessage.INFORM);
-            toClients.setContent(msg.getSender().getName() + ": " + msg.getContent());
-
             try {
+
+                //vyhledani klientu
                 DFAgentDescription[] clients = DFService.search(myAgent, dfd);
+
+                //vytvoreni zpravy pro klienty
+                ACLMessage toClients = new ACLMessage(ACLMessage.INFORM);
+                toClients.setContent(msg.getSender().getName() + ": " + msg.getContent());
+
+                //pridani vsech klientu mezi prijemce
                 for (DFAgentDescription client : clients) {
                     toClients.addReceiver(client.getName());
                 }
